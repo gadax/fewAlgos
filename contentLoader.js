87fafc03b08a05cf0875d9ea5264a3a8b905feb1
gadax <gabriel.danies@gmail.com>
2021-10-25 20:16:29 +0200

    remove onclick input property

--- contentLoader.js
+++ contentLoader.js
@@ -1,51 +1,70 @@
-if (window.fetch)	// fetch
-{
-	document.getElementsByTagName('a').forEach((ele) => {
-		ele.addEventListener('click', (e) => {
-			e.preventDefault();
-			fetch('https://gadax.github.io/fewAlgos/' + e.target.getAttribute('href')).then(function(response) {
-				return response.text();
+window.addEventListener('load', () => {
+	if (window.fetch)	// fetch
+		{
+			for(ele of document.getElementsByTagName('a'))
+			{
+				ele.addEventListener('click', (e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					fetch('https://gadax.github.io/fewAlgos/' + e.target.getAttribute('href')).then(function(response) {
+						return response.text(); // .text()
 
-			}).then(function(body) {
-				document.getElementById('dynContent').innerHTML = body;
-			});
-		});
-	});
+					}).then(function(body) {
+						document.getElementById('dynContent').innerHTML = body;
+						//console.log('§' + body);
+						//console.log(document.getElementById('dynContent'));
+					});
+				});
+			}
+			// document.getElementsByTagName('a').forEach((ele) => {
+			// 	ele.addEventListener('click', (e) => {
+			// 		e.preventDefault();
+			// 		e.stopPropagation();
+			// 		fetch('https://gadax.github.io/fewAlgos/' + e.target.getAttribute('href')).then(function(response) {
+			// 			return response.body; // .text()
 
-}
-else	// XMLHttpRequest
-{
-	// (function includeHTML()
-	// {
-	// 	var z, i, elmnt, file, xhttp;
-	// 	z = document.getElementsByTagName('*');/*querySelector('*[include-html]');*/
+			// 		}).then(function(body) {
+			// 			//document.getElementById('dynContent').innerHTML = body;
+			// 			console.log('-' + body);
+			// 		});
+			// 	});
+			// });
 
-	// 	for (i = 0; i < z.length; i++)
-	// 	{
-	// 		elmnt = z[i];
-	// 		file = elmnt.getAttribute('include-html');
+		}
+	else	// XMLHttpRequest
+	{
+		// (function includeHTML()
+		// {
+		// 	var z, i, elmnt, file, xhttp;
+		// 	z = document.getElementsByTagName('*');/*querySelector('*[include-html]');*/
 
-	// 		if (file)
-	// 		{
-	// 			xhttp = new XMLHttpRequest();
-	// 			xhttp.onreadystatechange = function()
-	// 			{
-	// 				if (this.readyState == 4)
-	// 				{
-	// 					if (this.status == 200) elmnt.innerHTML = this.responseText;
-	// 					if (this.status == 404) elmnt.innerHTML = 'Page not found.';
+		// 	for (i = 0; i < z.length; i++)
+		// 	{
+		// 		elmnt = z[i];
+		// 		file = elmnt.getAttribute('include-html');
 
-	// 					elmnt.removeAttribute('include-html');
-	// 					includeHTML();
-	// 				}
-	// 			}			
-	// 			xhttp.open('GET', file, true);
-	// 			xhttp.send();
-	// 			return;
-	// 		}
-	// 	}
-	// })();
-}
+		// 		if (file)
+		// 		{
+		// 			xhttp = new XMLHttpRequest();
+		// 			xhttp.onreadystatechange = function()
+		// 			{
+		// 				if (this.readyState == 4)
+		// 				{
+		// 					if (this.status == 200) elmnt.innerHTML = this.responseText;
+		// 					if (this.status == 404) elmnt.innerHTML = 'Page not found.';
+
+		// 					elmnt.removeAttribute('include-html');
+		// 					includeHTML();
+		// 				}
+		// 			}			
+		// 			xhttp.open('GET', file, true);
+		// 			xhttp.send();
+		// 			return;
+		// 		}
+		// 	}
+		// })();
+	}
+});
 
 
 
